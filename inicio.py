6de61e5bb759a7e576a6d182d45ae6799d7921b1
juseroal <juseroal@gmail.com>
2024-08-10 17:42:55 +0000

    pregunto el nombre

--- inicio.py
+++ inicio.py
@@ -5,5 +5,11 @@ if edad < 18:
 else:
     print("Puedes entrar")
 
+edad = int(input("¿Qué edad tienes? "))
+nombre = input("¿Cómo te llamas? ")
+if edad < 18:
+    print(nombre + ", no puedes entrar")
+else:
+    print(nombre + ", puedes entrar")
 
 
